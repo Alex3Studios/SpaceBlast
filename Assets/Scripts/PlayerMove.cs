493d4512d8fab6a2052ddcfb77aa6e32c91543ba
Allifreyr <Allifreyr@gmail.com>
2018-12-04 11:55:54 +0000

    Fixed the code up a bit, moved scripts to their folder etc...

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -12,32 +12,32 @@ public class PlayerMove : MonoBehaviour {
  
      //Update Function
      void Update () {
-                 //Move
-                 float Move = Input.GetAxis ("Horizontal");
-                 float MoveVertical = Input.GetAxis ("Vertical");
- 
-                 GetComponent<Rigidbody2D>().velocity = new Vector2 (MoveVertical * PlayerSpeed, GetComponent<Rigidbody2D>().velocity.x);
-                 GetComponent<Rigidbody2D>().velocity = new Vector2 (Move * PlayerSpeed, GetComponent<Rigidbody2D>().velocity.x);
-                 GetComponent<Rigidbody2D>().AddTorque (Move * PlayerRotation);
-                 
-                 //Flip
-                 if (Move > 0 && !IsRight) {
-                         Flip ();
-                 } else if (Move < 0 && IsRight) {
-                         Flip ();
-                 }
-                 
-                 //Jump
- //                if (Input.GetKeyDown (KeyCode.W)) {
- //                    rigidbody2D.AddForce(new Vector2 (0, JumpForce));
- //                }
+        //Move
+        float Move = Input.GetAxis ("Horizontal");
+        float MoveVertical = Input.GetAxis ("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (MoveVertical * PlayerSpeed, GetComponent<Rigidbody2D>().velocity.x);
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (Move * PlayerSpeed, GetComponent<Rigidbody2D>().velocity.x);
+        GetComponent<Rigidbody2D>().AddTorque (Move * PlayerRotation);
+        
+        //Flip
+        if (Move > 0 && !IsRight) {
+                Flip ();
+        } else if (Move < 0 && IsRight) {
+                Flip ();
+        }
+        
+        //Jump
+//                if (Input.GetKeyDown (KeyCode.W)) {
+//                    rigidbody2D.AddForce(new Vector2 (0, JumpForce));
+//                }
          }
  
      //Flip Function
      void Flip() {
          if (CanFlip) {
-                         IsRight = !IsRight;
-                         transform.Rotate(0f, 180f, 0f);
-                 }
+                IsRight = !IsRight;
+                transform.Rotate(0f, 180f, 0f);
+        }
      }
 }
