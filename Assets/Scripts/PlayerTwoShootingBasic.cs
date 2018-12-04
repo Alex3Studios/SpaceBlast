cb7eb49040b8b41e5bc58325eb460fb9a0d78c35
alexve18 <alexandergud18@gmail.com>
2018-12-04 14:03:36 +0000

    Added Rewired Assets and got controllers working

---
+++ Assets/Scripts/PlayerTwoShootingBasic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class PlayerTwoShootingBasic : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    private Player player2;
+
+    void Awake()
+    {
+        player2 = ReInput.players.GetPlayer(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (player2.GetButtonDown("Shoot"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
