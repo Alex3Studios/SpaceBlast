d1fafe1880da1a8e04e386a08b2cf41c7521a176
jollylovisa <alexandrawinther@gmail.com>
2018-12-05 16:11:52 +0000

    asteroids got movement + layer ordered + collision corrected :bowtie::panda_face:

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour {
+
+	public float maxThrust;
+	public float maxTorque;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		//Add a random amount of torque and thrust to the asteroid
+		Vector2 thrust =  new Vector2(Random.Range(-maxThrust, maxThrust), Random.Range(-maxThrust, maxThrust));
+		float torque = Random.Range(-maxTorque, maxTorque);
+		rb.AddForce(thrust);
+		rb.AddTorque(torque);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Physics2D.IgnoreLayerCollision(9,10);
+	}
+}
