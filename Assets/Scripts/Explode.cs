3c02263288666f91646f7431096eca384dbf73cc
alexve18 <alexandergud18@gmail.com>
2018-12-06 13:54:39 +0000

    Basic Rocket explosion is ready but we should really add a more cool effect

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    private GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+         explosion = (GameObject)Resources.Load("Prefab/Ammo/Explosion", typeof(GameObject));
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
