a3f07cd44f2c2f84d0eea81d968009c600bd393b
Allifreyr <Allifreyr@gmail.com>
2018-12-10 17:03:35 +0000

    Explosion sound added, new script for it and it no longer instantly disappear when it hits something with collision.

---
+++ Assets/Scripts/Weapons/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public int damage;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        PlayerOne playerOne = hitInfo.GetComponent<PlayerOne>();
+        PlayerTwo playerTwo = hitInfo.GetComponent<PlayerTwo>();
+
+        if (playerTwo != null)
+        {
+            playerTwo.TakeDamage(damage);
+        }
+        else if (playerOne != null)
+        {
+            playerOne.TakeDamage(damage);
+        }
+    }
+}
