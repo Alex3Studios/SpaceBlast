f85947df96f6752534e2aefab05880d41c401805
jollylovisa <alexandrawinther@gmail.com>
2018-12-12 14:36:04 +0000

    game over menu + button functions + script + refactor start menu

---
+++ Assets/Scripts/Scene/GameOverMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour {
+
+	// Called when Play Again button is pressed
+	public void PlayAgain () {
+		SceneManager.LoadScene("Main");
+	}
+
+	// Called when Main Menu button is pressed
+	public void MainMenu () {
+		SceneManager.LoadScene("StartMenu");
+	}
+}
