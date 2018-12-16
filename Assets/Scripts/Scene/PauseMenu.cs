c7bfe6142f9d306577d20a802cf45d5b5c550db0
jollylovisa <alexandrawinther@gmail.com>
2018-12-16 21:33:32 +0000

    comments in pausemenu script

--- Assets/Scripts/Scene/PauseMenu.cs
+++ Assets/Scripts/Scene/PauseMenu.cs
@@ -9,7 +9,11 @@ public class PauseMenu : MonoBehaviour {
 	public GameObject pauseMenuUI;
 	public GameObject helpMenuUI;
 	
-	// Update is called once per frame
+	/**************************************************************
+	 * Loads Play Menu Panel when 'Start'/'Esc' button is pressed *
+	 * If the game is paused then it resumes the game when the    *
+	 * button is pressed, and if not then it brings up the panel  *
+	 *************************************************************/
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Escape)) {
 			if(gameIsPaused) {
@@ -21,29 +25,43 @@ public class PauseMenu : MonoBehaviour {
 		}
 	}
 
+	/***************************************************
+	 * Resumes gameplay and sets the time back in play *
+	 **************************************************/
 	public void Resume() {
 		pauseMenuUI.SetActive(false);
 		Time.timeScale = 1f;
 		gameIsPaused = false;
 	} 
 
+	/*********************************************
+	 * Pauses gameplay and sets the time to stop *
+	 ********************************************/
 	void Pause() {
 		pauseMenuUI.SetActive(true);
 		Time.timeScale = 0f;
 		gameIsPaused = true;
 	}
 
+	/**************************************************************
+	 * Deactivates pause menu and brings up the help menu instead *
+	 *************************************************************/
 	public void Help() {
-		//Debug.Log("Help is selected");
 		pauseMenuUI.SetActive(false);
 		helpMenuUI.SetActive(true);
 	}
 
+	/************************************************
+	 * Leaves help menu and goes back to pause menu *
+	 ***********************************************/
 	public void quitHelp() {
 		helpMenuUI.SetActive(false);
 		pauseMenuUI.SetActive(true);
 	}
 
+	/******************************************************
+	 * Restart time, leave pause Menu and load Start Menu *
+	 *****************************************************/
 	public void quitGame() {
 		pauseMenuUI.SetActive(false);
 		Time.timeScale = 1f;
